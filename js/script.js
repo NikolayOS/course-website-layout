8d69dfcc564412698f198dfbd261be25d79f9b01
Mykola Osadchii <nikolayosadchii842@gmail.com>
2022-08-21 14:58:20 +0300

    add adaptive header. count down

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $( document).ready(function() {
     $('.header__burger').click(function(event){
-        $('.header__burger,.header-menu').toggleClass('active');
+        $('.header__burger,.header__menu,.cabinet').toggleClass('active');
         $('body').toggleClass('lock');
     });
     });
